2fc596dad1439c5d52a03bdf763d8b0d38aa9bab
bougyman <bougyman@users.noreply.github.com>
2024-10-23 15:42:58 -0600

    feat(state)!: Adds state management component (#74)
    * feat(state): Add State Management component
    
    * test(state): Adds State Management component specs

---
+++ lib/dapr/client/state.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require_relative '../client'
+
+module Rubyists
+  module Dapr
+    module Client
+      # The State class is a client for the Dapr State Management Building block
+      # Info here: https://docs.dapr.io/developing-applications/building-blocks/state-management/
+      class State
+        # Include the client module
+        include Client
+        include SemanticLogger::Loggable
+
+        attr_reader :store_name
+
+        Empty                = Google::Protobuf::Empty
+        Runtime              = Client::Runtime
+        GetBulkStateResponse = Runtime::GetBulkStateResponse
+        GetStateRequest      = Runtime::GetBulkStateRequest
+        SaveStateRequest     = Runtime::SaveStateRequest
+        DEFAULT_STORE_NAME   = 'statestore'
+
+        # @param keys       [Array<String>] keys to retrieve from the state store
+        # @param store_name [String]        name of the State Management component, defaults to 'dapr-statestore'
+        # @param metadata   [Hash]          metadata to include
+        #
+        # @return [GetBulkStateResponse] The response from the Dapr State Management component
+        def self.get(*keys, store_name: DEFAULT_STORE_NAME, metadata: {})
+          new(store_name).get(keys, metadata:)
+        end
+
+        # @param states     [Hash]   states to set in the state store, key/value pairs
+        # @param store_name [String] name of the State Management component, defaults to 'dapr-statestore'
+        # @param metadata   [Hash]   metadata to include
+        #
+        # @return [Empty] The response from the Dapr State Management component
+        def self.set(states, store_name: DEFAULT_STORE_NAME, metadata: {})
+          new(store_name).set(states, metadata:)
+        end
+
+        # Initialize the State Management client
+        #
+        # @param store_name [String]        name of the Dapr Configuration component to use
+        def initialize(store_name)
+          @store_name = store_name
+        end
+
+        # @param keys       [Array<String>] keys to retrieve from the state store
+        # @param metadata   [Hash]          metadata to include
+        #
+        # @return [Array<BulkStateItem>] Array of items returned by the state store
+        def get(keys, metadata: {})
+          logger.debug('Getting state', keys:, store_name:, metadata:)
+          response = singleton.get_bulk_state GetStateRequest.new(store_name:, keys:, metadata:)
+          response.items
+        end
+
+        # @param states     [Hash] states to set (key/values in the state store)
+        # @param metadata   [Hash] metadata to include
+        def set(states, metadata: {})
+          states  = states.map { |key, value| { key:, value:, metadata: } }
+          request = SaveStateRequest.new(store_name:, states:)
+          logger.debug('Setting state', states:, store_name:)
+          singleton.save_state request
+        end
+      end
+    end
+  end
+end
---
+++ test/dapr/client/test_state.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+# Tests for the Dapr State Management component
+class TestDaprState < Minitest::Test
+  require Rubyists::Dapr::LIBROOT / :client / :state
+  DummyClient = Rubyists::Dapr::Client::DummyClient
+  Runtime = ::Dapr::Proto::Runtime::V1
+  Common = ::Dapr::Proto::Common::V1
+  Empty = Google::Protobuf::Empty
+
+  def setup_dummy_client!
+    DummyClient.define_method(:states) { @states ||= {} }
+    define_get_bulk_state_method!
+    define_set_bulk_state_method!
+  end
+
+  def define_get_bulk_state_method!
+    DummyClient.define_method(:get_bulk_state) do |*args, &_block|
+      items = args.shift.keys.map.with_index do |key, index|
+        metadata = { key: "METAKEY-#{index}", value: "METAVALUE-#{index}" }
+        data = "test-state-data-#{index}"
+        Runtime::BulkStateItem.new(key:, data:, etag: '1', error: nil, metadata:)
+      end
+      Runtime::GetBulkStateResponse.new(items:)
+    end
+  end
+
+  def define_set_bulk_state_method!
+    DummyClient.define_method(:save_state) do |*args, &_block|
+      args.shift.states.map { |s| states[s.key] = s.value }
+      Empty
+    end
+  end
+
+  def make_it_testy!
+    return if testy?
+
+    setup_dummy_client!
+    @testy = true
+  end
+
+  def testy?
+    @testy
+  end
+
+  context 'State' do # rubocop:disable Metrics/BlockLength
+    should 'Read existing State item' do
+      make_it_testy!
+      messages = semantic_logger_events(Rubyists::Dapr::Client::State) do
+        states = Rubyists::Dapr::Client::State.get('test_item1', 'test_item2')
+
+        assert_equal 2, states.size
+        assert_equal 'test-state-data-0', states[0].data
+        assert_equal 'test_item1', states[0].key
+        assert_equal 'test_item2', states[1].key
+        assert_equal 'test-state-data-1', states[1].data
+      end
+
+      assert_equal 1, messages.size
+      assert_semantic_logger_event messages.first, level: :debug, message: 'Getting state'
+      refute_nil messages.first.payload[:keys]
+    end
+
+    should 'Set a State item' do
+      make_it_testy!
+      messages = semantic_logger_events(Rubyists::Dapr::Client::State) do
+        items = { 'test_item1' => 'test-state-data-writes-0', 'test_item2' => 'test-state-data-writes-1' }
+        response = Rubyists::Dapr::Client::State.set(items)
+
+        assert_equal Empty, response
+      end
+
+      assert_equal 1, messages.size
+      assert_semantic_logger_event messages.first, level: :debug, message: 'Setting state'
+      assert_equal [{ key: 'test_item1', value: 'test-state-data-writes-0', metadata: {} },
+                    { key: 'test_item2', value: 'test-state-data-writes-1', metadata: {} }],
+                   messages.first.payload[:states]
+    end
+  end
+end
